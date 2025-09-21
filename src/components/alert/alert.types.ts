74c7e69fc79c4d4fb7322606f30eb006efc43450
Mandana Dashti <dmandana59@gmail.com>
2025-09-21 20:54:37 +0330

    feat: create alert type

---
+++ src/components/alert/alert.types.ts
@@ -0,0 +1,7 @@
+import type {ComponentProps} from "react";
+
+export type AlertProps =ComponentProps<"div">&{
+    title?: string,
+    description?: string,
+    type?: "info" | "success" | "warning" | "error",
+}
