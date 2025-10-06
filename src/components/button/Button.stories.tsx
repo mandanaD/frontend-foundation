343402678f5c5d8f03fe10778b7c867842a89e5b
Mandana Dashti <dmandana59@gmail.com>
2025-10-06 23:19:09 +0330

    feat: change btn path

--- src/components/button/Button.stories.tsx
+++ src/components/button/Button.stories.tsx
@@ -1,6 +1,6 @@
 import type {Meta, StoryObj} from '@storybook/react';
+import {Button} from "./Button.tsx";
 
-import {Button} from './button';
 type Story = StoryObj<typeof Button>;
 
 const meta: Meta<typeof Button> = {
