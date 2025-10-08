996b8b9659a22d68d93620504bbf441e803df13d
Mandana Dashti <dmandana59@gmail.com>
2025-10-08 23:12:05 +0330

    feat: badge story

---
+++ src/components/Badge/Badge.stories.tsx
@@ -0,0 +1,62 @@
+import type {Meta, StoryObj} from '@storybook/react';
+import {Badge} from "./Badge.tsx";
+
+
+type Story = StoryObj<typeof Badge>
+
+
+const meta: Meta<typeof Badge> = {
+    title: 'Components/Badge',
+    component: Badge,
+    args: {
+        variant:"primary",
+        children:"Badge"
+    },
+    argTypes: {
+        variant: {
+            control: "select"
+        }
+    },
+}
+export default meta;
+
+
+export const Primary: Story = {
+    args: {
+        variant: 'primary',
+        children: 'Primary Badge',
+
+    },
+};
+export const Secondary: Story = {
+    args: {
+        variant: 'secondary',
+        children: 'Secondary Badge',
+    },
+};
+export const Info: Story = {
+    args: {
+        variant: 'info',
+        children: 'Info Badge',
+    },
+};
+export const Success: Story = {
+    args: {
+        variant: 'success',
+        children: 'Success Badge',
+    }
+};
+
+export const Warning: Story = {
+    args: {
+        variant: 'warning',
+        children: 'Warning Badge',
+    }
+};
+
+export const Error: Story = {
+    args: {
+        variant: 'error',
+        children: 'Error Badge',
+    }
+};
