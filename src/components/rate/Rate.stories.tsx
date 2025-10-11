068969db4e95cb3e7f6e04470ed7cf90020eeedd
Mandana Dashti <dmandana59@gmail.com>
2025-10-11 23:51:38 +0330

    feat: create Story component

---
+++ src/components/rate/Rate.stories.tsx
@@ -0,0 +1,68 @@
+import type {Meta, StoryObj} from '@storybook/react';
+import {Rate} from "./Rate.tsx";
+
+type Story = StoryObj<typeof Rate>;
+
+const meta: Meta<typeof Rate> = {
+    title: 'Components/Rate',
+    component: Rate,
+    args: {
+        size: "md",
+        shape: "starBold",
+        length:5
+    },
+    argTypes: {
+        shape: {
+            control: "select"
+        },
+        size: {
+            control: "select"
+        },
+    },
+}
+export default meta;
+
+export const Squircle: Story = {
+    args: {
+        shape: 'squircle',
+    },
+};
+export const Heart: Story = {
+    args: {
+        shape: 'heart',
+    },
+};
+export const Hexagon: Story = {
+    args: {
+        shape: 'hexagon',
+    },
+};
+export const Hexagon2: Story = {
+    args: {
+        shape: 'hexagon2',
+    },
+};
+export const Decagon: Story = {
+    args: {
+        shape: 'decagon',
+    },
+};
+export const Pentagon: Story = {
+    args: {
+        shape: 'pentagon',
+    },
+};export const Star: Story = {
+    args: {
+        shape: 'star',
+    },
+};
+export const StarBold: Story = {
+    args: {
+        shape: 'starBold',
+    },
+};
+export const Circle: Story = {
+    args: {
+        shape: 'circle',
+    },
+};
