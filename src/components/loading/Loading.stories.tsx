b0c864b415eecc3c8405fda67528e12101a35847
Mandana Dashti <dmandana59@gmail.com>
2026-01-24 11:52:58 +0330

    feat: add AllSizes

--- src/components/loading/Loading.stories.tsx
+++ src/components/loading/Loading.stories.tsx
@@ -56,3 +56,42 @@ export const Infinity: Story = {
         shape: 'infinity',
     },
 };
+
+//sizes
+
+export const AllSizes: Story = {
+    render: (args) => (
+        <div className="flex gap-2 flex-wrap">
+            <Loading {...args} size="xs"/>
+            <Loading {...args} size="sm"/>
+            <Loading {...args} size="md"/>
+            <Loading {...args} size="lg"/>
+            <Loading {...args} size="xl"/>
+        </div>
+    )
+};
+export const Xs: Story = {
+    args: {
+        size: 'xs',
+    },
+};
+export const Md: Story = {
+    args: {
+        size: 'md',
+    },
+};
+export const Sm: Story = {
+    args: {
+        size: 'sm',
+    },
+};
+export const Lg: Story = {
+    args: {
+        size: 'lg',
+    },
+};
+export const Xl: Story = {
+    args: {
+        size: 'xl',
+    },
+};
